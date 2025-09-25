b177159ff3f455435cc3b1b16e7d0d5b9686b3f9
Zurab Shukakidze <zurashukakidze85@gmail.com>
2025-09-25 11:28:45 +0400

    Delete homework5 directory

--- homework5/task4.js
+++
@@ -1,8 +0,0 @@
-async function fetchAPI(API) {
-  let res = await fetch(API);
-  let data = await res.json();
-  let slicedData = data.slice(0, -6);
-  console.log(slicedData);
-}
-
-fetchAPI("https://jsonplaceholder.typicode.com/users ");
